c4c65ce883cb8a36eee8228fb18217d82110a290
SAM PAZOUKI VAROONEH <68926038+sam-pazouki@users.noreply.github.com>
2024-04-29 12:22:43 -0300

    Update Banner.jsx

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -7,8 +7,8 @@ function Banner() {
         <div className="w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-36">
           <div className="space-y-8">
             <h1 className="text-2xl md:text-4xl font-bold">
-              Hello, welcomes here to learn something{" "}
-              <span className="text-pink-500">new everyday!!!</span>
+              Hello, welcomes to our App{" "}
+              <span className="text-pink-500">Learn new things every single day!</span>
             </h1>
             <p className="text-sm md:text-xl">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
